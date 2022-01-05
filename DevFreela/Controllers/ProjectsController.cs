f9bda1141c43699efaa62dafbc9451e787b4ed7d
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2022-01-04 20:47:47 -0400

    feat: Created Users and its Actions

--- DevFreela/Controllers/ProjectsController.cs
+++ DevFreela/Controllers/ProjectsController.cs
@@ -50,5 +50,22 @@ namespace DevFreela.Controllers
             return NoContent();
         }
 
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, [FromBody] CreateCommentModel createCommentModel)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
     }
 }
---
+++ DevFreela/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserModel createUserModel)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, createUserModel);
+        }
+
+        [HttpPut("{id}/login")]
+        public IActionResult Login(int id, [FromBody] LoginModel loginModel)
+        {
+            return NoContent();
+        }
+    }
+}
