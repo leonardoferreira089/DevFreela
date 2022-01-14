96f6123706877c77bc49bf1b79e2e169489fc261
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2022-01-13 21:57:58 -0400

    feat: Created SkillService and Interface

--- DevFreela.Application/Services/Interfaces/ISkillService.cs
+++ DevFreela.Application/Services/Interfaces/ISkillService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevFreela.Application.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace DevFreela.Application.Services.Interfaces
 {
     public interface ISkillService
     {
+        List<SkillViewModel> GetAll();
     }
 }
