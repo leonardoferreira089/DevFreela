ca590f8b846e60931ec7ecd91d6c952513f57e10
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2022-01-13 21:44:43 -0400

    feat: Implemented ProjectService

--- DevFreela.Core/Entities/Project.cs
+++ DevFreela.Core/Entities/Project.cs
@@ -27,9 +27,43 @@ namespace DevFreela.Core.Entities
         public int IdClient { get; private set; }
         public int IdFreelancer { get; private set; }
         public decimal TotalCost { get; private set; }
+        public DateTime? StartedAt { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public DateTime FinishedAt { get; private set; }
         public ProjectStatusEnum Status { get; private set; }
         public List<ProjectComment> Comments { get; private set; }
+
+        public void Cancel()
+        {
+            if(Status == ProjectStatusEnum.InProgress)
+            {
+                Status = ProjectStatusEnum.Cancelled;
+            }
+        }
+
+        public void Start()
+        {
+            if(Status == ProjectStatusEnum.Created)
+            {
+                Status = ProjectStatusEnum.InProgress;
+                StartedAt = DateTime.Now;
+            }
+        }
+
+        public void Finish()
+        {
+            if(Status == ProjectStatusEnum.InProgress)
+            {
+                Status = ProjectStatusEnum.Finished;
+                FinishedAt = DateTime.Now;
+            }
+        }
+
+        public void Update(string title,string description, decimal totalCost)
+        {
+            Title = title;
+            Description = description;
+            TotalCost = totalCost;
+        }
     }
 }
