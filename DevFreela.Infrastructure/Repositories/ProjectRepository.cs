6c2285f53901c71d26e388d9578c97553f795e52
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2022-03-23 15:19:41 +0100

    refactor: Implemented Repository to GetAllProjectQuery

---
+++ DevFreela.Infrastructure/Repositories/ProjectRepository.cs
@@ -0,0 +1,25 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Repositories
+{
+    public class ProjectRepository
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        public ProjectRepository(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Project>> GetAllAsync()
+        {
+            return await _dbContext.Projects.ToListAsync();
+        }
+    }
+}
