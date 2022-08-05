2dd2f7706d8c1c8a4228f10e0f7781db155475ef
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2022-08-05 14:46:00 +0200

    API name

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,113 @@
+﻿using DevFreela.Application.Commands.CreateCommentCommand;
+using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Application.Commands.DeleteProject;
+using DevFreela.Application.Commands.FinishProject;
+using DevFreela.Application.Commands.StartProject;
+using DevFreela.Application.Commands.UpdateProject;
+using DevFreela.Application.InputModels;
+using DevFreela.Application.Queries.GetAllProjects;
+using DevFreela.Application.Queries.GetProjectById;
+using DevFreela.Models;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {        
+        private readonly IMediator _mediator;
+        public ProjectsController(IMediator mediator)
+        {            
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "client, freelancer")]
+        public async Task<IActionResult> Get(string query)
+        {
+            var getAllProjects = new GetAllProjectsQuery(query);
+            var projects = await _mediator.Send(getAllProjects);
+
+            return Ok(projects);
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(Roles = "client, freelancer")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var query = new GetProjectByIdQuery(id);
+            var project = await _mediator.Send(id);
+            if(project == null)
+            {
+                return NotFound();
+            }
+            return Ok(project);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "client")]
+        public async Task<IActionResult> Post([FromBody] CreateProjectCommand createProjectCommand)
+        {            
+            var id = await _mediator.Send(createProjectCommand);
+            return CreatedAtAction(nameof(GetById), new { id }, createProjectCommand);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "client")]
+        public async Task<IActionResult> Put(int id, [FromBody] UpdateProjectCommand command)
+        {
+            if (command.Description.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "client")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var command = new DeleteProjectCommand(id);
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpPost("{id}/comments")]
+        [Authorize(Roles = "client, freelancer")]
+        public async Task<IActionResult> PostComment(int id, [FromBody] CreateCommentCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpPut("{id}/start")]
+        [Authorize(Roles = "client")]
+        public async Task<IActionResult> Start(int id)
+        {
+            var command = new StartProjectCommand(id);
+
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpPut("{id}/finish")]
+        [Authorize(Roles = "client")]
+        public async Task<IActionResult> Finish(int id)
+        {
+            var command = new FinishProjectCommand(id);
+            await _mediator.Send(id);        
+            
+            return NoContent();
+        }
+    }
+}
